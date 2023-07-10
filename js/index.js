71d5457e3e28ebf8423d95b55b29404871283efb
JulianRDZ <rodriguezjuliann@outlook.com>
2023-07-10 19:52:47 -0300

    Segunda Pre Entrega

--- js/index.js
+++ js/index.js
@@ -17,6 +17,15 @@ function infoPrecio(codigo) {
         case 4:
             console.log("El plan BRONCE cuesta $5500")
             break;
+        case 5:
+            console.log("El plan NATACION cuesta $8000")
+            break;
+        case 6:
+            console.log("El plan MUSCULACION cuesta $8000")
+            break;
+        case 7:
+            console.log("El plan FITNESS cuesta $6000")
+            break;
         default:
             console.warn("No entendimos tu selección 🤔")
             break;
@@ -38,7 +47,7 @@ function infoPlanes() {
     let respuesta = confirm("¿Desea conocer el precio de algun plan?")
     if (respuesta === true) {
         let codigo = parseInt(prompt("Ingresa el codigo de plan: "))
-        if (codigo === 1 || codigo === 2 || codigo === 3 || codigo === 4) {
+        if (codigo === 1 || codigo === 2 || codigo === 3 || codigo === 4 || codigo === 5 || codigo === 6 ||codigo === 7) {
             infoPrecio(codigo)
             let respuestaComprar = confirm("¿Quiere comprar el plan? 🤑")
             if (respuestaComprar === true) {
@@ -56,5 +65,69 @@ function infoPlanes() {
         }
     } else {
         console.warn("👍No olvides consultarnos cuando lo necesites 👍")
+    }
+}
+
+// SEGUNDA PRE ENTREGA
+
+const carrito = []
+
+const planes = [{imagen: "💎", codigo: 1, nombre: "Plan DIAMANTE", precio: 12000},
+                {imagen: "🥇", codigo: 2, nombre: "Plan ORO", precio: 10000},
+                {imagen: "🥈", codigo: 3, nombre: "Plan PLATA", precio: 7000},
+                {imagen: "🥉", codigo: 4, nombre: "Plan BRONCE", precio: 5500},
+                {imagen: "🏊‍♂️", codigo: 5, nombre: "Plan NATACION", precio: 8000},
+                {imagen: "🏋️‍♀️", codigo: 6, nombre: "Plan MUSCULACION", precio: 8000},
+                {imagen: "🤸", codigo: 7, nombre: "Plan FITNESS", precio: 6000}]
+
+function buscarPlanes(codigo) {
+    let resultado = planes.find((plan)=> plan.codigo === parseInt(codigo))
+    return resultado 
+}
+
+class Compra {
+    constructor(carritoDeCompras) {
+        this.carrito = carritoDeCompras
+    }
+    obtenerSubtotal() {
+        if(this.carrito.length > 0) {
+            return this.carrito.reduce((acc, plan)=> acc + plan.precio, 0)
+        }
+    }
+}
+
+function verPlanes(){
+    let planesCod = planes.map((plan)=>{
+        return{
+            codigo: plan.codigo,
+            nombre: plan.nombre
+        }
+    })
+    console.table(planesCod)
+}
+
+function finalizarCompra(){
+    const shopping = new Compra(carrito)
+    console.log("El total de su compra es " + shopping.obtenerSubtotal())
+}
+
+function comprar() {
+    let codigo = prompt("Ingresa el codigo del plan:")
+    let planElegido = buscarPlanes(codigo)
+    if (planElegido !== undefined) {
+        carrito.push(planElegido)
+        alert("Se ha agregado " + planElegido.nombre + " al carrito. 💪🏻")
+        let respuesta = confirm("¿Quieres comprar algun otro plan?")
+        if (respuesta === true) {
+            comprar()
+        } else {
+            console.warn("No olvides de finalizar tu compra")
+            console.table(carrito)
+            finalizarCompra()
+            infoCuotas()
+            alert("✅ En unos momentos lo contactaremos para finalizar su compra. Muchas gracias!")
+        }
+    } else {
+        alert("Error en el plan ingresado. Vuelva a intentarlo desde el comienzo")
     }
 }
